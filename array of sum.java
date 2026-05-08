22feece09d588e9555a6f2c97ffd9b3543661df1
tulasikothari2809 <tulasi.kothari2809@gmail.com>
2026-05-08 17:38:18 +0800

    Create array of sum.java

---
+++ array of sum.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+
+        System.out.println("Sum of array = " + sum);
+    }
+}
